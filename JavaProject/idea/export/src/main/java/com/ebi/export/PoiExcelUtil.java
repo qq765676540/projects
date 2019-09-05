fead8b5ef1a42980cc201bfce887970e869e4cfb
yuchengquan <765676540@qq.com>
2019-09-05 18:49:07 +0800

    update

--- JavaProject/idea/export/src/main/java/com/ebi/export/PoiExcelUtil.java
+++ JavaProject/idea/export/src/main/java/com/ebi/export/PoiExcelUtil.java
@@ -3,31 +3,31 @@ package com.ebi.export;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
 
-@RestController
-@CrossOrigin
-public class Controller {
-    @RequestMapping(value = "/hello")
-    public String hello() {
-        return "hello,Spring Boot";
+public class PoiExcelUtil {
+    public static void main(String[] args) throws IOException {
+        PoiExcelUtil poi = new PoiExcelUtil();
+        poi.exportExcel("d:\\test.xlsx");
     }
 
-    @GetMapping(value = "export")
-    public String export(String str) throws IOException {
+    public void exportExcel(String str) throws IOException {
+        //读取excel
         File file = new File(str);
         InputStream input = null;
         input = new FileInputStream(file);
         Workbook workbook = new XSSFWorkbook(input);
         Sheet sheet = workbook.getSheetAt(0);
-        sheet.shiftRows(0, 10, 2);
-        Row newRow0 = sheet.createRow(0);
+        //插入行
+//        sheet.shiftRows(0, 10, 2);
+//        Row newRow0 = sheet.createRow(0);
+        //合并单元格
+        CellRangeAddress region = new CellRangeAddress(1, 1, 0, 2);
+        sheet.addMergedRegion(region);
+        //保存关闭
         OutputStream output = null;
         output = new FileOutputStream(str);
         workbook.write(output);
@@ -37,6 +37,5 @@ public class Controller {
         file = null;
         input = null;
         output = null;
-        return str;
     }
 }
