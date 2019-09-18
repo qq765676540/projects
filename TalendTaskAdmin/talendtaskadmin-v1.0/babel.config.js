2d0364efb2920ded945e2aecdfbb2f8f36b2f2f7
yuchengquan <765676540@qq.com>
2019-09-18 09:28:27 +0800

    update

--- TalendTaskAdmin/talendtaskadmin-v1.0/babel.config.js
+++ TalendTaskAdmin/talendtaskadmin-v1.0/babel.config.js
@@ -1,5 +1,17 @@
+const webpack = require('webpack');
+
 module.exports = {
-  presets: [
-    '@vue/app'
-  ]
+  outputDir: 'D:/VUEProject/TalenTaskAdmin',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default']
+      })
+    ]
+  }
 }
