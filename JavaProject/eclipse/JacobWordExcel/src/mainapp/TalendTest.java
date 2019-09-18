79d13e0a93c8b188817451dc1147db4a426cf88e
yuchengquan <765676540@qq.com>
2019-09-18 09:10:50 +0800

    update

---
+++ JavaProject/eclipse/JacobWordExcel/src/mainapp/TalendTest.java
@@ -0,0 +1,20 @@
+package mainapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class TalendTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a = "13:49";
+		String b = "13:50";
+		System.out.println(a.compareTo(b));
+		
+
+	}
+
+}
