6e94286d59456918855d2ad9896cc32336463f06
yuchengquan <765676540@qq.com>
2020-05-23 12:38:09 +0800

    update

---
+++ TalendTaskAdmin/talendtaskadmin-v2.0/src/js/test.js
@@ -0,0 +1,6 @@
+let arr1 = [[1,2,3],[2,3,4]];
+let arr2 = [[5,6,7],[6,7,8]];
+let arra = [arr1,arr2];
+let arrb = [...arr1,...arr2];
+console.log('YCQ日志记录:标识->',arra);
+console.log('YCQ日志记录:标识->',arrb);
