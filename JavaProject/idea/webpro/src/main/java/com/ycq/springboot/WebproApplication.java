40dd38962f3fa5066d9dfe8d7b631c7beba17587
yuchengquan <765676540@qq.com>
2020-06-01 10:25:02 +0800

    update

---
+++ JavaProject/idea/webpro/src/main/java/com/ycq/springboot/WebproApplication.java
@@ -0,0 +1,16 @@
+package com.ycq.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@SpringBootApplication
+public class WebproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebproApplication.class, args);
+    }
+
+}
---
+++ JavaProject/idea/webpro/src/main/java/com/ycq/springboot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.ycq.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello")
+    public @ResponseBody String hello(){
+        return "helle spring boot";
+    }
+}
