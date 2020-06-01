53cb8c04e08ba87fc4ebd6ebd7307b0afae7effa
yuchengquan <765676540@qq.com>
2020-06-01 16:04:47 +0800

    update

--- JavaProject/idea/webpro/src/main/java/com/ycq/springboot/WebproApplication.java
+++ JavaProject/idea/webpro/src/main/java/com/ycq/springboot/WebproApplication.java
@@ -1,7 +1,14 @@
 package com.ycq.springboot;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
--- JavaProject/idea/webpro/src/main/java/com/ycq/springboot/controller/HelloController.java
+++ JavaProject/idea/webpro/src/main/java/com/ycq/springboot/controller/HelloController.java
@@ -1,10 +1,10 @@
 package com.ycq.springboot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @RequestMapping(value = "/hello")
     public @ResponseBody String hello(){
